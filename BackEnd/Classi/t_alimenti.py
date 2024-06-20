7757b4c49f66a32577501998913c5e8803328e7d
Pepperu93 <pepperusso93@icloud.com>
2024-06-20 16:01:23 +0200

    controlli

--- BackEnd/Classi/t_alimenti.py
+++ BackEnd/Classi/t_alimenti.py
@@ -58,59 +58,65 @@ class t_alimenti:
         key = request.args
         if 'id' not in key:
             return {'Error':'Wrong Key'}, 403
-        id = request.args.get('id')
         try:
-            self.Cursor.callproc('get_t_alimenti', [id])
-            for results in self.Cursor.stored_results():
-                result = results.fetchone()
-                (self.ID, self.Alimento, self.Parte_Edibile_Gr, self.Energia_Kcal, self.Energia_KJ, self.Acqua_Gr, self.Prot_Tot_Gr,
-                self.Prot_Anim, self.Prot_Veg, self.Glucidi_Tot, self.Amido, self.Glucidi_Solub, self.Lipidi_Tot, self.Saturi_Tot,
-                self.Monoins_Tot, self.Polins_Tot, self.Ac_Oleico, self.Ac_Linoleico, self.Ac_Linolenico, self.Altri_Polins, self.Colesterolo,
-                self.Fibre_Alim, self.Alcool, self.Ferro, self.Ca, self.Na, self.K, self.P, self.Zn, self.Vit_B1, self.Vit_B2,
-                self.Vit_B3, self.Vit_C, self.Vit_B6, self.Folico, self.Retinolo_Eq, self.Beta_Carotene, self.Vit_E, self.Vit_D) = result
-                self.DB.commit_transaction()
-                lista = []
-                lista.append({
-                    'id': self.ID,
-                    'alimento': self.Alimento,
-                    'parte_edibile_gr': self.Parte_Edibile_Gr,
-                    'energia_kcal': self.Energia_Kcal,
-                    'acqua_gr': self.Acqua_Gr,
-                    'prot_tot_gr': self.Prot_Tot_Gr,
-                    'prot_anim': self.Prot_Anim,
-                    'prot_veg': self.Prot_Veg,
-                    'glucidi_tot': self.Glucidi_Tot,
-                    'lipidi_tot': self.Lipidi_Tot,
-                    'saturi_tot': self.Saturi_Tot,
-                    'monoins_tot': self.Monoins_Tot,
-                    'polins_tot': self.Polins_Tot,
-                    'ac_oleico': self.Ac_Oleico,
-                    'ac_linoleico': self.Ac_Linoleico,
-                    'ac_linoleico': self.Ac_Linolenico,
-                    'altri_polins': self.Altri_Polins,
-                    'colesterolo': self.Colesterolo,
-                    'fibre_alim': self.Fibre_Alim,
-                    'alcool': self.Alcool,
-                    'ferro': self.Ferro,
-                    'ca': self.Ca,
-                    'na': self.Na,
-                    'k': self.K,
-                    'p': self.P,
-                    'zn': self.Zn,
-                    'vit_b1': self.Vit_B1,
-                    'vit_b2': self.Vit_B2,
-                    'vit_b3': self.Vit_B3,
-                    'vit_c': self.Vit_C,
-                    'vit_b6': self.Vit_B6,
-                    'folico': self.Folico,
-                    'retinolo_eq': self.Retinolo_Eq,
-                    'beta_carotene': self.Beta_Carotene,
-                    'vit_e': self.Vit_E,
-                    'vit_d': self.Vit_D
-                })
-                return lista
-        except Exception as e:
-            print(e)
-            self.DB.rollback_transaction()
-            self.DB.close_connection()
-            return {'Error': str(e)}, 500
+            id = int(request.args.get('id'))
+        except:
+            return {'Error': 'id deve essere un intero'}, 403
+        if (isinstance(id, int)):
+            try:
+                self.Cursor.callproc('get_t_alimenti', [id])
+                for results in self.Cursor.stored_results():
+                    result = results.fetchone()
+                    (self.ID, self.Alimento, self.Parte_Edibile_Gr, self.Energia_Kcal, self.Energia_KJ, self.Acqua_Gr, self.Prot_Tot_Gr,
+                    self.Prot_Anim, self.Prot_Veg, self.Glucidi_Tot, self.Amido, self.Glucidi_Solub, self.Lipidi_Tot, self.Saturi_Tot,
+                    self.Monoins_Tot, self.Polins_Tot, self.Ac_Oleico, self.Ac_Linoleico, self.Ac_Linolenico, self.Altri_Polins, self.Colesterolo,
+                    self.Fibre_Alim, self.Alcool, self.Ferro, self.Ca, self.Na, self.K, self.P, self.Zn, self.Vit_B1, self.Vit_B2,
+                    self.Vit_B3, self.Vit_C, self.Vit_B6, self.Folico, self.Retinolo_Eq, self.Beta_Carotene, self.Vit_E, self.Vit_D) = result
+                    self.DB.commit_transaction()
+                    lista = []
+                    lista.append({
+                        'id': self.ID,
+                        'alimento': self.Alimento,
+                        'parte_edibile_gr': self.Parte_Edibile_Gr,
+                        'energia_kcal': self.Energia_Kcal,
+                        'acqua_gr': self.Acqua_Gr,
+                        'prot_tot_gr': self.Prot_Tot_Gr,
+                        'prot_anim': self.Prot_Anim,
+                        'prot_veg': self.Prot_Veg,
+                        'glucidi_tot': self.Glucidi_Tot,
+                        'lipidi_tot': self.Lipidi_Tot,
+                        'saturi_tot': self.Saturi_Tot,
+                        'monoins_tot': self.Monoins_Tot,
+                        'polins_tot': self.Polins_Tot,
+                        'ac_oleico': self.Ac_Oleico,
+                        'ac_linoleico': self.Ac_Linoleico,
+                        'ac_linoleico': self.Ac_Linolenico,
+                        'altri_polins': self.Altri_Polins,
+                        'colesterolo': self.Colesterolo,
+                        'fibre_alim': self.Fibre_Alim,
+                        'alcool': self.Alcool,
+                        'ferro': self.Ferro,
+                        'ca': self.Ca,
+                        'na': self.Na,
+                        'k': self.K,
+                        'p': self.P,
+                        'zn': self.Zn,
+                        'vit_b1': self.Vit_B1,
+                        'vit_b2': self.Vit_B2,
+                        'vit_b3': self.Vit_B3,
+                        'vit_c': self.Vit_C,
+                        'vit_b6': self.Vit_B6,
+                        'folico': self.Folico,
+                        'retinolo_eq': self.Retinolo_Eq,
+                        'beta_carotene': self.Beta_Carotene,
+                        'vit_e': self.Vit_E,
+                        'vit_d': self.Vit_D
+                    })
+                    return lista
+            except Exception as e:
+                print(e)
+                self.DB.rollback_transaction()
+                self.DB.close_connection()
+                return {'Error': str(e)}, 500
+        else:
+            return {'Error': 'id deve essere un intero'}, 400
