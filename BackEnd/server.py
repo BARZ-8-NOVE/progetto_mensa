8079db20915e24c71b80397261803fc35e721ec9
michele <michele.baruzzi@be-team.eu>
2024-06-27 11:44:29 +0200

    t_alimenti sviluppati in modo corretto

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from Classi.ClasseDB.config import DATABASE_URI
 from Classi.ClasseDB.db_connection import get_db, Base, engine
-from Classi.ClasseAlimenti.Classe_t_alimenti import TAlimenti
+from Classi.ClasseAlimenti.Classe_t_alimenti.Controller_t_alimenti import t_alimenti_controller
 from Classi.ClasseUtenti.Classe_t_funzionalita.Controller_t_funzionalita import t_funzionalita_controller
 
 app = Flask(__name__)
@@ -13,22 +13,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = engine
 
-@app.route('/alimenti/<int:id>', methods=['GET'])
-def get_alimenti(id):
-    session = sessionmaker(bind=db)()
-    t_alimenti_instance = TAlimenti()  # Creare un'istanza di TAlimenti
-    response = t_alimenti_instance.get_t_alimenti_by_id(session, id)  # Chiamare il metodo sull'istanza
-    session.close()  # Chiudi la sessione dopo l'uso
-    return jsonify(response), 200 if 'Error' not in response else 404
 
-@app.route('/alimenti/all', methods=['GET'])
-def get_all_alimenti():
-    session = sessionmaker(bind=db)()
-    t_alimenti_instance = TAlimenti()  # Creare un'istanza di TAlimenti
-    response = t_alimenti_instance.get_all(session)  # Chiamare il metodo sull'istanza
-    session.close()  # Chiudi la sessione dopo l'uso
-    return jsonify(response), 200 if 'Error' not in response else 404
-
+app.register_blueprint(t_alimenti_controller, url_prefix='/alimenti')
 app.register_blueprint(t_funzionalita_controller, url_prefix='/funzionalita')
 
 if __name__ == '__main__':
