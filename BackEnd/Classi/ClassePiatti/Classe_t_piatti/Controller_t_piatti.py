cb025d3961d98f83670945fa8363e8a8f7ede18d
michele <michele.baruzzi@be-team.eu>
2024-07-02 11:54:20 +0200

    fix piatti

--- BackEnd/Classi/ClassePiatti/Classe_t_piatti/Controller_t_piatti.py
+++ BackEnd/Classi/ClassePiatti/Classe_t_piatti/Controller_t_piatti.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from Classi.ClassePiatti.Classe_t_piatti.Service_t_piatti import ServicePiatti
+from datetime import datetime
 
 t_piatti_controller = Blueprint('piatti', __name__)
 service_piatti = ServicePiatti()
@@ -17,7 +18,7 @@ def get_by_id(id):
 @t_piatti_controller.route('/create', methods=['POST'])
 def create():
     dati = request.json
-    required_fields = ['fkTipoPiatto', 'fkServizio', 'codice', 'titolo', 'descrizione', 'inMenu', 'ordinatore', 'dataInserimento', 'utenteInserimento', 'dataCancellazione', 'utenteCancellazione']
+    required_fields = ['fkTipoPiatto', 'fkServizio', 'codice', 'titolo', 'descrizione', 'inMenu', 'ordinatore', 'utenteInserimento']
     if not all(field in dati for field in required_fields):
         return jsonify({'Error': 'wrong keys!'}), 403
     try:
@@ -25,15 +26,13 @@ def create():
         fkServizio = int(dati['fkServizio'])
         codice = dati['codice'].strip()
         titolo = dati['titolo'].strip()
-        descrizione = dati['descrizione'].strip()
+        descrizione = dati['descrizione'].strip() if dati.get('descrizione') else None
         inMenu = bool(dati['inMenu'])
         ordinatore = int(dati['ordinatore'])
-        dataInserimento = dati['dataInserimento']  # Assumi che sia una stringa già nel formato corretto
+        dataInserimento = dati.get('dataInserimento', datetime.now())
         utenteInserimento = dati['utenteInserimento'].strip()
-        dataCancellazione = dati['dataCancellazione']  # Assumi che sia una stringa già nel formato corretto
-        utenteCancellazione = dati['utenteCancellazione'].strip()
 
-        return jsonify(service_piatti.create(fkTipoPiatto, fkServizio, codice, titolo, descrizione, inMenu, ordinatore, dataInserimento, utenteInserimento, dataCancellazione, utenteCancellazione))
+        return jsonify(service_piatti.create(fkTipoPiatto, fkServizio, codice, titolo, descrizione, inMenu, ordinatore, dataInserimento, utenteInserimento))
 
     except ValueError as ve:
         return jsonify({'Error': str(ve)}), 403
@@ -43,21 +42,24 @@ def create():
 @t_piatti_controller.route('/update/<int:id>', methods=['PUT'])
 def update(id):
     dati = request.json
-    required_fields = ['fkTipoPiatto', 'fkServizio', 'codice', 'titolo', 'descrizione', 'inMenu', 'ordinatore', 'dataInserimento', 'utenteInserimento', 'dataCancellazione', 'utenteCancellazione']
-    if not all(field in dati for field in required_fields):
-        return jsonify({'Error': 'wrong keys!'}), 403
+    required_fields = ['fkTipoPiatto', 'fkServizio', 'codice', 'titolo', 'inMenu', 'ordinatore', 'dataInserimento', 'utenteInserimento']
+    
+    for field in required_fields:
+        if field not in dati or dati[field] is None:
+            return jsonify({'Error': f'{field} is required and cannot be None'}), 403
+    
     try:
         fkTipoPiatto = int(dati['fkTipoPiatto'])
         fkServizio = int(dati['fkServizio'])
         codice = dati['codice'].strip()
         titolo = dati['titolo'].strip()
-        descrizione = dati['descrizione'].strip()
+        descrizione = dati['descrizione'].strip() if dati.get('descrizione') else None
         inMenu = bool(dati['inMenu'])
         ordinatore = int(dati['ordinatore'])
-        dataInserimento = dati['dataInserimento']  # Assumi che sia una stringa già nel formato corretto
+        dataInserimento = dati.get('dataInserimento', datetime.now())
         utenteInserimento = dati['utenteInserimento'].strip()
-        dataCancellazione = dati['dataCancellazione']  # Assumi che sia una stringa già nel formato corretto
-        utenteCancellazione = dati['utenteCancellazione'].strip()
+        dataCancellazione = dati.get('dataCancellazione')  # Assumi che sia una stringa già nel formato corretto
+        utenteCancellazione = dati.get('utenteCancellazione', '').strip() if dati.get('utenteCancellazione') else None
 
         return jsonify(service_piatti.update(id, fkTipoPiatto, fkServizio, codice, titolo, descrizione, inMenu, ordinatore, dataInserimento, utenteInserimento, dataCancellazione, utenteCancellazione))
 
@@ -69,7 +71,7 @@ def update(id):
 @t_piatti_controller.route('/delete/<int:id>', methods=['DELETE'])
 def delete(id):
     dati = request.json
-    if 'utenteCancellazione' not in dati:
+    if 'utenteCancellazione' not in dati or dati['utenteCancellazione'] is None:
         return jsonify({'Error': 'utenteCancellazione key missing!'}), 403
     try:
         utenteCancellazione = dati['utenteCancellazione'].strip()
