60a8c880024cc090f392128bcfdcff7fc56edb16
Pepperu93 <pepperusso93@icloud.com>
2024-07-04 17:33:19 +0200

    commit

--- BackEnd/Classi/ClasseAlimenti/Classe_t_tipologiaalimenti/Domani_t_tipologiaalimenti.py
+++ BackEnd/Classi/ClasseAlimenti/Classe_t_tipologiaalimenti/Domani_t_tipologiaalimenti.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from Classi.ClasseDB.db_connection import Base
-from ..Classe_t_tipologiaconservazione.Domani_t_tipologiaconservazione import TTipologiaConservazioni
+from Classi.ClasseAlimenti.Classe_t_tipologiaconservazione.Domani_t_tipologiaconservazione import TTipologiaConservazioni
 
 class TTipologiaAlimenti(Base):
     __tablename__ = 't_tipologiaalimenti'
@@ -9,5 +9,3 @@ class TTipologiaAlimenti(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
     fktipologiaConservazione = Column(Integer, ForeignKey('t_tipologiaconservazione.id'))
-
-
